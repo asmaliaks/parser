ae6175dc76494243a885a35e5b231fadfbf1bc16
asmalouski <asmaliaks@gmail.com>
2019-07-22 10:12:19 +0300

    Minor changes

--- ebextentions/scripts/mysql-install.sh
+++ ebextentions/scripts/mysql-install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 cd ~/
-wget http://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
-dpkg -i mysql-apt-config_0.8.9-1_all.deb
-apt-get update
-apt-get install mysql-server
-mysql_secure_installation
+yum install mysql-server -y
 
